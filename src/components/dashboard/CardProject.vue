90a3bbd03aad1dd686a790a3f741bdf29862f41b
ndkag <duykhang02vnn@gmail.com>
2025-01-08 23:01:57 +0700

    u

---
+++ src/components/dashboard/CardProject.vue
@@ -0,0 +1,82 @@
+<template>
+    <router-link to="/" class="max-w-full overflow-hidden bg-white">
+        <!-- Phần hình ảnh -->
+        <div class="relative rounded-xl overflow-hidden">
+            <img class="w-full h-96 object-cover" :src="imageUrl || 'https://placehold.co/600x380'" :alt="title" />
+            <!-- Progress bar -->
+            <div class="absolute bottom-0 left-0 right-0 bg-black/40 p-1 pl-5 m-2 rounded-full flex gap-2">
+                <div class="w-full flex flex-col justify-end mb-2">
+                    <div class="flex items-center justify-between text-white text-[13px] font-semibold mb-0.5">
+                        <span>{{ formattedNumber(amount) }} đ</span>
+                        <span>{{ progress }}%</span>
+                    </div>
+                    <div class="w-full bg-gray-200 rounded-full h-[4px]">
+                        <div class="bg-gradient-to-r from-orange-300 via-orange-500 to-red-500 h-[4px] rounded-full" :style="{ width: `${progress}%` }"></div>
+                    </div>
+                </div>
+                <div class="min-w-12 h-12 bg-[#FFFAF5] rounded-full">
+                    <img src="/Image/iconDonate.png" alt="#" class="w-full h-full" />
+                </div>
+            </div>
+        </div>
+
+        <!-- Phần nội dung -->
+        <div class="p-4">
+            <h3 class="font-semibold text-gray-700 text-lg mb-2 line-clamp-2">{{ title }}</h3>
+            <div class="flex items-center justify-between text-gray-600">
+                <span>{{ supporters }} lượt ủng hộ</span>
+                <span>Còn lại {{ formattedNumber(daysLeft) }} ngày</span>
+            </div>
+        </div>
+    </router-link>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+defineProps({
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: [Number, String],
+        required: true
+    },
+    progress: {
+        type: Number,
+        default: 0
+    },
+    supporters: {
+        type: [Number, String],
+        required: true
+    },
+    daysLeft: {
+        type: [Number, String],
+        required: true
+    }
+});
+
+const formattedNumber = (value) => {
+    return new Intl.NumberFormat('vi-VN').format(value);
+};
+</script>
+
+<style>
+/* Thay đổi màu sắc và độ mờ của thanh tiến trình */
+.bg-black {
+    background-color: rgba(0, 0, 0, 0.7);
+}
+
+.bg-red-600 {
+    background-color: #e3342f;
+}
+
+.bg-gray-300 {
+    background-color: #d1d5db;
+}
+</style>
--- src/components/landing/TopbarWidget.vue
+++ src/components/landing/TopbarWidget.vue
@@ -2,14 +2,14 @@
 import { ref } from 'vue';
 
 function smoothScroll(id: any) {
-  document.body.click()
-  const element = document.getElementById(id)
-  if (element) {
-    element.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    })
-  }
+    document.body.click();
+    const element = document.getElementById(id);
+    if (element) {
+        element.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }
 }
 const items = ref([
     {
@@ -18,14 +18,12 @@ const items = ref([
             {
                 label: 'New',
                 icon: 'pi pi-plus',
-                command: () => {
-                }
+                command: () => {}
             },
             {
                 label: 'Print',
                 icon: 'pi pi-print',
-                command: () => {
-                }
+                command: () => {}
             }
         ]
     },
@@ -35,100 +33,91 @@ const items = ref([
             {
                 label: 'New',
                 icon: 'pi pi-plus',
-                command: () => {
-                }
+                command: () => {}
             },
             {
                 label: 'Print',
                 icon: 'pi pi-print',
-                command: () => {
-                }
+                command: () => {}
             }
         ]
-    },{
+    },
+    {
         label: 'Khám phá',
         items: [
             {
                 label: 'New',
                 icon: 'pi pi-plus',
-                command: () => {
-                }
+                command: () => {}
             },
             {
                 label: 'Print',
                 icon: 'pi pi-print',
-                command: () => {
-                }
+                command: () => {}
             }
         ]
-    },{
+    },
+    {
         label: 'Giới thiệu',
         items: [
             {
                 label: 'New',
                 icon: 'pi pi-plus',
-                command: () => {
-                }
+                command: () => {}
             },
             {
                 label: 'Print',
                 icon: 'pi pi-print',
-                command: () => {
-                }
+                command: () => {}
             }
         ]
-    },
+    }
 ]);
 </script>
 
 <template>
-  <a class="flex items-center w-[180px] h-[37px] mr-4" href="#">
-    <img src="/public/Image/logo.png" alt="" class="h-full object-contain w-full">
-  </a>
-  <Button
-    class="lg:!hidden"
-    text
-    severity="secondary"
-    rounded
-    v-styleclass="{
-      selector: '@next',
-      enterFromClass: 'hidden',
-      enterActiveClass: 'animate-scalein',
-      leaveToClass: 'hidden',
-      leaveActiveClass: 'animate-fadeout',
-      hideOnOutsideClick: true,
-    }"
-  >
-    <i class="pi pi-bars !text-2xl"></i>
-  </Button>
-  <div
-    class="items-center bg-surface-0 dark:bg-surface-900 grow justify-between hidden lg:flex absolute lg:static w-full left-0 top-full px-12 lg:px-0 z-20 rounded-border"
-  >
-  <IconField >
-    <InputIcon class="pi pi-search customInputIcon" />
-    <InputText  placeholder="Tìm kiếm tên chiến dịch" class="rounded-full customerInput" size="large"/>
-</IconField>
-  <div class="flex">
-    <Menubar :model="items" class="text-base font-medium" />
+    <a class="flex items-center w-[180px] h-[37px] mr-4" href="#">
+        <img src="/Image/logo.png" alt="" class="h-full object-contain w-full" />
+    </a>
+    <Button
+        class="lg:!hidden"
+        text
+        severity="secondary"
+        rounded
+        v-styleclass="{
+            selector: '@next',
+            enterFromClass: 'hidden',
+            enterActiveClass: 'animate-scalein',
+            leaveToClass: 'hidden',
+            leaveActiveClass: 'animate-fadeout',
+            hideOnOutsideClick: true
+        }"
+    >
+        <i class="pi pi-bars !text-2xl"></i>
+    </Button>
+    <div class="items-center bg-surface-0 dark:bg-surface-900 grow justify-between hidden lg:flex absolute lg:static w-full left-0 top-full px-12 lg:px-0 z-20 rounded-border">
+        <IconField>
+            <InputIcon class="pi pi-search customInputIcon" />
+            <InputText placeholder="Tìm kiếm tên chiến dịch" class="rounded-full customerInput" size="large" />
+        </IconField>
+        <div class="flex">
+            <Menubar :model="items" class="text-base font-medium" />
 
-    <div class="flex border-t lg:border-t-0 border-surface py-4 lg:py-0 mt-4 lg:mt-0 gap-2">
-      <Button label="Tạo chiện dịch" severity="warn" ></Button>
-      <Button label="Đăng nhập" to="/login" as="router-link" text severity="warn" ></Button>
+            <div class="flex border-t lg:border-t-0 border-surface py-4 lg:py-0 mt-4 lg:mt-0 gap-2">
+                <Button label="Tạo chiện dịch" severity="warn"></Button>
+                <Button label="Đăng nhập" to="/login" as="router-link" text severity="warn"></Button>
+            </div>
+        </div>
     </div>
-  </div>
-
-  </div>
 </template>
 <style>
-.p-menubar{
-  border: none !important
+.p-menubar {
+    border: none !important;
 }
-.customInputIcon	{
-  color: orangered !important;
+.customInputIcon {
+    color: orangered !important;
 }
-.customerInput{
-  border-radius: 50px !important;
-
+.customerInput {
+    border-radius: 50px !important;
 }
-
 </style>
